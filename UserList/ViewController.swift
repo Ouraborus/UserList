5c2fcf74b10328bf54fc5472b2e8859b96e1afe9
Esteban Cardona <1222eca@gmail.com>
2021-02-06 11:43:18 -0500

    First Commit

---
+++ UserList/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UserList
+//
+//  Created by Esteban Cardona Alvarez on 6/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.backgroundColor = .black
+    }
+
+
+}
+
